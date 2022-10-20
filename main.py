5164304b5e6ba550b6566ce33ed8b16e5339fc00
Tymon <79155383+imustend@users.noreply.github.com>
2022-10-20 11:01:09 +0200

    fixed ya game

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import logging
-
-
-def initialize_logging() -> None:
-    fmt = "%(asctime)s [%(levelname)s] %(message)s"
-    logging.basicConfig(format=fmt, level=logging.NOTSET)
-
-
-initialize_logging()
+print("penis")
