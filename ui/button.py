2835df0f1b43b5f4e621b928ce2ed1e59dd2e59b
bk20dev <60577942+bk20dev@users.noreply.github.com>
2022-10-20 10:05:19 +0200

    Create buttons

---
+++ ui/button.py
@@ -0,0 +1,18 @@
+import logging
+
+import pygame
+
+
+class Button:
+    def __init__(self, size: (int, int), position: (int, int) = (0, 0)):
+        self.surface = pygame.surface.Surface(size)
+        self.position = position
+
+    def render(self, screen: pygame.Surface, color):
+        self.surface.fill(color)
+        screen.blit(self.surface, self.position)
+
+    def is_clicked(self, mouse: (int, int)) -> bool:
+        px, py = self.position
+        width, height = self.surface.get_size()
+        return mouse[0] in range(px, px+width) and mouse[1] in range(py, py+height)
